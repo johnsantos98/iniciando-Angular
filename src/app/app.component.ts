81e23c95df7bf042c2347b27b43779621e1cc719
Joaovitor <111459871+johnsantos98@users.noreply.github.com>
2023-02-16 15:01:38 -0300

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,12 +15,23 @@ export class AppComponent  {
     this.todos.push(new todo(1, 'Passear com o cachorro', false));
     this.todos.push(new todo(2, 'ir ao supermercado', false));
     this.todos.push(new todo(3, 'corta o cabelo', true));
-
-
+  }
+  remove(todo: todo){
+  const index = this.todos.indexOf(todo);
+  if(index !== - 1){
+    this.todos.splice(index, 1);
 
   }
+  }
 
+markAsDone(todo: todo){
+todo.done= true;
+}
 
+markAsUndone(todo: todo){
+  todo.done = false;
+
+}
 
 
 }
