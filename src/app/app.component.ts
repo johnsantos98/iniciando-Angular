0e6254e8582c5dff3e5b0dfae01953371559cd54
Joaovitor <111459871+johnsantos98@users.noreply.github.com>
2023-02-16 14:32:51 -0300

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { todo } from './../models/todo.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent  {
+  public todos: todo[]= [];
+  public title: String = 'Minhas tarefas';
+
+
+  constructor() {
+    this.todos.push(new todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new todo(2, 'ir ao supermercado', false));
+    this.todos.push(new todo(3, 'corta o cabelo', true));
+
+
+
+  }
+
+
+
+
 }
+
+
---
+++ src/models/todo.model.ts
@@ -0,0 +1,12 @@
+export class todo {
+
+  constructor(
+    public id: number= 1,
+    public title : String,
+    public done: boolean,
+
+  ) {
+
+
+  }
+}
